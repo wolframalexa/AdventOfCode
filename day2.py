dce5cc90e8e85c6dd95099061227cdcf5316b5a1
wolframalexa <alexa.jakob4@gmail.com>
2019-12-02 12:23:36 -0500

    cleaned up

--- day2.py
+++ day2.py
@@ -1,28 +1,24 @@
 f = open('day2input.txt', "r")
 lines = f.readlines()
-numbers_original = map(int, lines[0].split(','))
-numbers = numbers_original
+numbers = map(int, lines[0].split(','))
+
 
 # Part 1: Figure out the code at 0
-
 i = 0
-print len(numbers)
 while i < len(numbers):
   if numbers[i] == 1:
     numbers[numbers[i+3]] = numbers[numbers[i+1]] + numbers[numbers[i+2]]
   elif numbers[i] == 2:
     numbers[numbers[i+3]] = numbers[numbers[i+1]]*numbers[numbers[i+2]]
   elif numbers[i] == 99:
-    print "Program stopped, answer:",numbers[0]
+    print "Part 1 answer: ",numbers[0]
     break
   i = i + 4
 
 # Part 2: Find inputs that produce the output 19690720
-
-
 for noun in range(100):
   for verb in range(100):
-    numbers = map(int, lines[0].split(','))
+    numbers =  map(int, lines[0].split(',')) # reset numbers to the input at every loop
     numbers[1] = noun
     numbers[2] = verb
 
@@ -40,5 +36,6 @@ for noun in range(100):
       print "Noun: ",noun
       print "Verb: ",verb
       answer = 100*noun+verb
-      print "The answer is: ",answer
+      print "The answer for part 2 is: ",answer
+      break
 f.close()
