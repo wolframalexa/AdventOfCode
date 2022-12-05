60eed495b5b7bb4b53b1b4899386e774a9cb2441
Alexa Jakob <alexa.jakob@formlabs.com>
2022-12-05 09:24:13 -0500

    day 5, learned about deepcopy

--- 2022/day5.py
+++ 2022/day5.py
@@ -1,3 +1,4 @@
+import copy
 f = open("day5input.txt")
 lines = f.readlines()
 
@@ -9,20 +10,23 @@ for line in lines[:8]:
 		if line[i] != "    " and line[i] != "   \n":
 			init_stacks[i+1].append(''.join(c for c in line[i] if c.isalnum()))
 
-stacks = init_stacks
-stacks2 = init_stacks
+stacks = copy.deepcopy(init_stacks)
+stacks2 = copy.deepcopy(init_stacks)
 # parse instructions
 for instr in lines[10:]:
 	instr = [int(s) for s in instr.split() if s.isdigit()]
 	for i in range(1, instr[0]+1): # number of times to repeat
 		crate = stacks[instr[1]].pop(0) # pop crate from origin
 		stacks[instr[2]].insert(0, crate) # put crate on destination
+print("After part 1:", stacks)
 
 for instr in lines[10:]:
 	instr = [int(s) for s in instr.split() if s.isdigit()]
+	print(instr)
 	crates = stacks2[instr[1]][:instr[0]] # get first n crates
 	stacks2[instr[1]] = stacks2[instr[1]][instr[0]:] # remove those crates from the list
 	stacks2[instr[2]] = crates + stacks2[instr[2]]
+	print(stacks2)
 
-print("After part 1:", stacks)
+
 print("After part 2:", stacks2)
