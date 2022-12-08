d915171b56dc64a8e0add9183e62c158fcd927be
wolframalexa <alexa.jakob4@gmail.com>
2022-12-07 17:58:29 -0800

    finish day 7

--- 2022/day7.py
+++ 2022/day7.py
@@ -1,19 +1,32 @@
+from collections import defaultdict
+
 f = open("day7input.txt")
 cmd = f.readlines()
 
-files = {}
+files = defaultdict(int)
 path = []
 
 for line in cmd:
-    line = line.strip().split(" ")
-    print(line)
-    if line[1] == "cd":
-        pass
-    elif line[1] == "ls":
-        pass
-    elif line[0] == "dir":
-        pass
-    else: # size, file
-        print("size, file")
+	line = line.strip().split(" ")
+	if line[1] == "cd":
+		if line[2] == "..":
+			path.pop()
+		elif line[2] == "/":
+			path = ["/"]
+		else:
+			path.append(line[2])
+	elif line[1] == "ls":
+		pass
+	elif line[0] == "dir":
+		pass
+	else: # size, file
+		for i in range(len(path)):
+			k = "/".join(path[:i+1])
+			files[k] += int(line[0])
 
-# TODO add handling for each of the above cases
+new = dict(filter(lambda files: files[1] <= 100000, files.items()))
+print("Part 1:", sum(new.values()))
+
+target = 30000000 - (70000000 - files["/"])
+todelete = min(dict(filter(lambda files: files[1] > target, files.items())).values())
+print("Part 2:", todelete)
