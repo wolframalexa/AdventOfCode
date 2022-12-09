97cf5d07a23fffdf3b801b1737c145c5b32c9c29
wolframalexa <alexa.jakob4@gmail.com>
2022-12-08 16:31:06 -0800

    part 1 debugging

--- 2022/day8.py
+++ 2022/day8.py
@@ -1,15 +1,17 @@
 import numpy as np
+import sys
+np.set_printoptions(threshold=sys.maxsize)
 
 def findVisible(trees):
 	visible = np.zeros_like(trees)
 	for i in range(len(trees)):
-	highest = trees[i][0]
-	visible[:][i,0] = 1
+		highest = trees[i][0]
+		visible[:][i,0] = 1
 
-	for j in range(len(trees2[i])):
-		if trees2[i][j] > highest:
-			visible[:][i,j] = 1
-			highest = trees2[i][j]
+		for j in range(len(trees2[i])):
+			if trees2[i][j] > highest:
+				visible[:][i,j] = 1
+				highest = trees2[i][j]
 	return(visible)
 
 f = open("day8input.txt", "r")
@@ -21,24 +23,19 @@ for row in trees:
 	row = row.strip("\n")
 	trees2.append([int(x) for x in [*row]])
 trees2 = np.array(trees2)
-print(trees2)
-
-visible = np.zeros_like(trees2)
 
 # from the left
-for i in range(len(trees2)):
-	highest = trees2[i][0]
-	visible[:][i,0] = 1
+left_vis = findVisible(trees2)
 
-	for j in range(len(trees2[i])):
-		if trees2[i][j] > highest:
-			visible[:][i,j] = 1
-			highest = trees2[i][j]
 # from the right
-for i in range(len(trees2)):
-	highest = trees2[i][-1]
-	visible[:][i,-1] = 1
+right_vis = findVisible(np.fliplr(trees2))
 
-	for j in range(len(trees2[i],0)):
+# from the bottom
+bottom_vis = findVisible(np.transpose(trees2))
 
-print(visible)
+# from the bottom
+top_vis = findVisible(np.fliplr(np.transpose(trees2)))
+
+vis = left_vis + np.fliplr(right_vis) + np.transpose(top_vis) + np.transpose(np.fliplr(bottom_vis))
+#print(vis)
+print(np.count_nonzero(vis))
