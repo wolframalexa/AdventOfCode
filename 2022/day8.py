72e1c9a67e92d6bc5902c0f4a0e12db51493c444
wolframalexa <alexa.jakob4@gmail.com>
2022-12-08 16:11:58 -0800

    first pass

---
+++ 2022/day8.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+f = open("day8input.txt", "r")
+
+trees = f.readlines()
+
+trees2 = []
+for row in trees:
+	row = row.strip("\n")
+	trees2.append([int(x) for x in [*row]])
+trees2 = np.array(trees2)
+print(trees2)
+
+visible = np.zeros_like(trees2)
+
+# from the left
+for i in range(len(trees2)):
+	highest = trees2[i][0]
+	visible[:][i,0] = 1
+
+	for j in range(len(trees2[i])):
+		if trees2[i][j] > highest:
+			visible[:][i,j] = 1
+			highest = trees2[i][j]
+# from the right
+for i in range(len(trees2)):
+	highest = trees2[i][-1]
+	visible[:][i,-1] = 1
+
+	for j in range(len(trees2[i],0)):
+
+print(visible)
