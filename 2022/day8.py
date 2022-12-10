3d8d4c21905e6bb491cb41321478f6b75d494f86
wolframalexa <alexa.jakob4@gmail.com>
2022-12-10 13:41:36 -0800

    pair programming with alex for day 9

--- 2022/day8.py
+++ 2022/day8.py
@@ -1,21 +1,45 @@
 import numpy as np
-import sys
-np.set_printoptions(threshold=sys.maxsize)
 
-def findVisible(trees):
-	visible = np.zeros_like(trees)
-	for i in range(len(trees)):
-		highest = trees[i][0]
+def score2(data,h,flip=False):
+	try:
+		if len(data) > 1 and flip:
+			data = data[::-1]
+	except:
+		pass
+	x = np.where(data>=h)[0]
+	if x.size != 0:
+		return x[0]+1
+	try:
+		return len(data)
+	except:
+		return 1
+
+def score(data,x,y):
+	a = 0
+	b = 0
+	c = 0
+	d = 0
+
+	h = data[x,y]
+	a = score2(data[x+1:,y].squeeze(),h)
+	b = score2(data[:x,y].squeeze(),h,True)
+	c = score2(data[x,y+1:].squeeze(),h)
+	d = score2(data[x,:y:].squeeze(),h,True)
+	return a*b*c*d
+
+def findVisible(data):
+	visible = np.zeros_like(data)
+	for i in range(len(data)):
+		highest = data[i][0]
 		visible[:][i,0] = 1
 
-		for j in range(len(trees2[i])):
-			if trees2[i][j] > highest:
+		for j in range(len(data[i])):
+			if data[i][j] > highest:
 				visible[:][i,j] = 1
-				highest = trees2[i][j]
-	return(visible)
+				highest = data[i][j]
+	return visible
 
 f = open("day8input.txt", "r")
-
 trees = f.readlines()
 
 trees2 = []
@@ -23,6 +47,16 @@ for row in trees:
 	row = row.strip("\n")
 	trees2.append([int(x) for x in [*row]])
 trees2 = np.array(trees2)
+high = 0
+best = 0
+for i in range(trees2.shape[0]):
+	for j in range(trees2.shape[1]):
+		n = score(trees2,i,j)
+		if n > high:
+			best = (i,j)
+			high = n
+print(best)
+print("Part 2:", high)
 
 # from the left
 left_vis = findVisible(trees2)
@@ -30,12 +64,12 @@ left_vis = findVisible(trees2)
 # from the right
 right_vis = findVisible(np.fliplr(trees2))
 
-# from the bottom
-bottom_vis = findVisible(np.transpose(trees2))
+# from the top
+top_vis = findVisible(np.rot90(trees2))
 
+bottom_vis = findVisible(np.fliplr(np.rot90(trees2)))
 # from the bottom
-top_vis = findVisible(np.fliplr(np.transpose(trees2)))
-
-vis = left_vis + np.fliplr(right_vis) + np.transpose(top_vis) + np.transpose(np.fliplr(bottom_vis))
-#print(vis)
-print(np.count_nonzero(vis))
+print(np.rot90(np.fliplr(top_vis)))
+vis = left_vis + np.fliplr(right_vis) + np.fliplr(np.rot90(bottom_vis)) + np.fliplr(np.rot90(np.fliplr(top_vis)))
+print(vis)
+print("Part 1:", np.count_nonzero(vis))
